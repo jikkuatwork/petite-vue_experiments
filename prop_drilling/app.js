11476568b711c7d1e01a9dd2bb1777863b278bda
Jikku Jose <jikku@storybrain.com>
2024-05-10 00:39:46 +0530

    Move getRatio to different file

--- prop_drilling/app.js
+++ prop_drilling/app.js
@@ -1,34 +1,5 @@
 import { createApp, reactive } from "https://cdn.skypack.dev/petite-vue@0.4.1"
-
-export async function getRatio(tickerOne, tickerTwo) {
-  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${tickerOne},${tickerTwo}&tsyms=USD`
-  try {
-    const response = await fetch(url, {
-      headers: {},
-      body: null,
-      method: "GET",
-      mode: "cors",
-      credentials: "omit",
-    })
-    const data = await response.json()
-    if (
-      data.RAW &&
-      data.RAW[tickerOne] &&
-      data.RAW[tickerTwo] &&
-      data.RAW[tickerOne].USD &&
-      data.RAW[tickerTwo].USD
-    ) {
-      const priceOne = data.RAW[tickerOne].USD.PRICE
-      const priceTwo = data.RAW[tickerTwo].USD.PRICE
-      const ratio = priceOne / priceTwo
-      return Math.round((ratio + Number.EPSILON) * 100) / 100 // Rounding to two decimal places
-    } else {
-      throw new Error("Data for one or both tickers is missing")
-    }
-  } catch (error) {
-    return null
-  }
-}
+import { getRatio } from "./getRatio.js"
 
 function Row({ tickers, key, editMode, handleRemove }) {
   return {
@@ -150,6 +121,7 @@ const store = reactive({
     this.show.value = !this.show.value
   },
 })
+
 createApp({
   Button,
   store,
