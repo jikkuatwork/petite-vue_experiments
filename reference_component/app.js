23c7954fa4d30b8c7c68559545b429557a794d09
Jikku Jose <jikku@storybrain.com>
2024-05-16 22:58:50 +0530

    Clear boilerplate

--- reference_component/app.js
+++ reference_component/app.js
@@ -1,9 +1,3 @@
 import { createApp } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js"
 
-import { store } from "./store.js"
-import { Avatar } from "./components/Avatar/index.js"
-
-createApp({
-  Avatar,
-  store,
-}).mount()
+createApp({}).mount()
