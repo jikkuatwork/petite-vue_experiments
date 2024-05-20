d420383f8708970a9b4c6530729ae2a2d99000d0
Jikku Jose <jikku@storybrain.com>
2024-05-20 13:41:27 +0530

    Make theming possible

--- reference_component/components/Toggle.js
+++ reference_component/components/Toggle.js
@@ -2,7 +2,7 @@
  * Petite Vue Reference Component Implementation
  *
  * Specs:
- * 1. Must return an object
+ * 1. Must return an object & should have `export`
  * 2. Will receive arguments as object
  * 3. Returned object *must* have all variables and functions needed in the template
  * 4. Component can have local state but mostly it will be a PURE component
@@ -10,20 +10,22 @@
  * 6. `$template`'s value will be prepended by `\/* HTML *\/` for prettier
  * 7. Props must have default values, with functions logging arguments
  * 8. All state will be managed outside the component
+ * 9. Classes can be programattically generated but will
  *
  * */
 
 export const Toggle = ({
   isOn = { value: false },
   handleChange = () => console.log("state: ", isOn),
+  themeColor = "red-600",
 } = {}) => {
   const $template = /* HTML */ `
-    <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
+    <!-- Enabled: "bg-{{ themeColor }}", Not Enabled: "bg-gray-200" -->
     <button
       @click="handleChange"
       type="button"
-      :class="{ 'bg-indigo-600': isOn.value, 'bg-gray-200': !isOn.value }"
-      class=" relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+      :class="{ [\`bg-${themeColor} focus:ring-${themeColor}\`] : isOn.value, 'bg-gray-200': !isOn.value }"
+      class=" relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2  focus:ring-offset-2"
       role="switch"
       aria-checked="false"
     >
@@ -40,6 +42,7 @@ export const Toggle = ({
   return {
     isOn,
     handleChange,
+    themeColor,
     $template,
   }
 }
