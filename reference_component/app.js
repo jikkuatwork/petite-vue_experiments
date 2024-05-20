0e3f186bfa582de69c904441ba618707a795dc37
Jikku Jose <jikku@storybrain.com>
2024-05-20 15:16:06 +0530

    Implement theme change in template

--- reference_component/app.js
+++ reference_component/app.js
@@ -3,16 +3,43 @@ import {
   reactive,
 } from "https://cdn.jsdelivr.net/npm/petite-vue@0.4.1/+esm"
 
-import { Toggle } from "./components/Toggle.js"
+import { Cell } from "./components/Toggle.js"
 
 const store = reactive({
   isOn: { value: true },
   toggle() {
     this.isOn.value = !this.isOn.value
   },
+  themeColor: { value: "green-400" },
+  toggleTheme() {
+    if (this.themeColor.value == "green-400") {
+      this.themeColor.value = "pink-300"
+    } else {
+      this.themeColor.value = "green-400"
+    }
+  },
 })
 
+export const Button = ({
+  handleClick = () => console.log("Item Click!"),
+} = {}) => {
+  const label = "Toggle Theme"
+
+  const $template = /* HTML */ `
+    <div class="">
+      <div
+        class="p-2 bg-yellow-300 rounded cp select-none"
+        @click="handleClick"
+      >
+        {{ label }}
+      </div>
+    </div>
+  `
+  return { $template, label, handleClick }
+}
+
 createApp({
-  Toggle,
+  Cell,
+  Button,
   store,
 }).mount()
--- reference_component/components/Toggle.js
+++ reference_component/components/Toggle.js
@@ -2,29 +2,29 @@
  * Petite Vue Reference Component Implementation
  *
  * Specs:
- * 1. Must return an object & should have `export`
- * 2. Will receive arguments as object
- * 3. Returned object *must* have all variables and functions needed in the template
- * 4. Component can have local state but mostly it will be a PURE component
- * 5. Component will not depend on anything other than tailwind (v2.2.19) utility classes
- * 6. `$template`'s value will be prepended by `\/* HTML *\/` for prettier
- * 7. Props must have default values, with functions logging arguments
- * 8. All state will be managed outside the component
- * 9. Classes can be programattically generated but will
+ * 01. Must return an object & should have `export`
+ * 02. Will receive arguments as object
+ * 03. Returned object *must* have all variables and functions needed in template
+ * 04. Component can have local state but mostly it will be a PURE component
+ * 05. Component will not depend on anything except tailwind (v2.2.19)
+ * 06. `$template`'s value will be prepended by `\/* HTML *\/` for prettier
+ * 07. Props must have default values, with functions logging arguments
+ * 08. All state will be managed outside the component
+ * 09. Classes can be generated: code needs to be provided as *key values* for `:class`
  *
  * */
 
 export const Toggle = ({
   isOn = { value: false },
   handleChange = () => console.log("state: ", isOn),
-  themeColor = "red-600",
+  themeColor = { value: "red-600" },
 } = {}) => {
   const $template = /* HTML */ `
     <!-- Enabled: "bg-{{ themeColor }}", Not Enabled: "bg-gray-200" -->
     <button
       @click="handleChange"
       type="button"
-      :class="{ [\`bg-${themeColor} focus:ring-${themeColor}\`] : isOn.value, 'bg-gray-200': !isOn.value }"
+      :class="{ [buttonOn()] : isOn.value, 'bg-gray-200': !isOn.value }"
       class=" relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2  focus:ring-offset-2"
       role="switch"
       aria-checked="false"
@@ -44,5 +44,28 @@ export const Toggle = ({
     handleChange,
     themeColor,
     $template,
+    buttonOn() {
+      return `bg-${themeColor.value} focus:ring-${themeColor.value}`
+    },
+  }
+}
+
+export const Cell = ({ isOn, handleChange, themeColor } = {}) => {
+  const label = "Hello"
+
+  const $template = /* HTML */ `
+    <div class="w-full p-4 bg-gray-100">
+      <div
+        v-scope="Toggle({isOn: isOn, handleChange: handleChange, themeColor: themeColor})"
+      ></div>
+    </div>
+  `
+  return {
+    $template,
+    label,
+    Toggle,
+    isOn,
+    handleChange,
+    themeColor,
   }
 }
