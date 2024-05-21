e9f87da239ae39f733339014c3999b7439cd705c
Jikku Jose <jikku@storybrain.com>
2024-05-22 01:13:42 +0530

    Add experiments

--- app.js
+++ app.js
@@ -4,23 +4,10 @@ import {
 } from "https://cdn.jsdelivr.net/npm/petite-vue@0.4.1/+esm"
 
 import Card from "./components/Card.js"
+import { experiments } from "./experiments.js"
 
 const store = reactive({
-  experiments: [
-    { key: 0, title: "props", subTitle: "prop drilling", url: "/props/" },
-    {
-      key: 1,
-      title: "reference",
-      subTitle: "context for ChatGPT",
-      url: "/reference/",
-    },
-    {
-      key: 2,
-      title: "storybook",
-      subTitle: "dynamic loading of remote components",
-      url: "/storybook/",
-    },
-  ],
+  experiments,
 })
 
 createApp({ Card, store }).mount()
---
+++ experiments.js
@@ -0,0 +1,15 @@
+export const experiments = [
+  { key: 0, title: "props", subTitle: "prop drilling", url: "/props/" },
+  {
+    key: 1,
+    title: "reference",
+    subTitle: "context for ChatGPT",
+    url: "/reference/",
+  },
+  {
+    key: 2,
+    title: "remote_load",
+    subTitle: "dynamic loading of remote components",
+    url: "/remote_load/",
+  },
+]
